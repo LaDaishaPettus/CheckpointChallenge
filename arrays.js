192cc433c22ab6b32e6509e70ca1a566614be6a7
Ladaisha Pettus <ladaishapettus@Administrators-MacBook-Pro.local>
2019-08-19 16:05:48 -0400

    checkpoint challenges

---
+++ arrays.js
@@ -0,0 +1,26 @@
+//what type of data can be stored in an array? numbers, booleans, strings, objects
+
+//What is an associative array? using strings instead of index
+
+// what is a multidimensional array? Array of Arrays
+
+// var odd = [1, 3, 5, 7, 9];
+// var even = [2, 4, 6, 8, 10];
+// var allNums = [...odd, ...even];
+// console.log(allNums);
+// alert(allNums[4]);
+// alert(allNums[6]);
+
+// var myFarm = ["chickens", "pigs", "cows", "horses", "ostriches"];
+// myFarm.forEach(ani => {
+//   console.log(ani);
+// });
+// myFarm.filter(far => {
+//   if (far.charAt(0) == "c" || far.charAt(0) == "o") {
+//     alert(`${far} starts with 'c' or '0' `);
+//   }
+// });
+
+var rvColors = ["red", "black", "grey", "white"];
+var color = rvColors.join();
+console.log(color);
